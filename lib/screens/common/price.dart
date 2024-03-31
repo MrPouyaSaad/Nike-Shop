40b6e7cfb57921f4664bee390a731c52bc937664
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2024-03-31 12:28:45 +0330

    add price formant

--- lib/screens/common/price.dart
+++ lib/screens/common/price.dart
@@ -1,3 +1,8 @@
+import 'package:intl/intl.dart';
+
 extension PriceLable on int {
-  String get withPriceLabel => '$this تومان';
+  String get withPriceLabel => NumberFormat.currency(
+        decimalDigits: 0,
+        customPattern: '000,000 تومان',
+      ).format(this);
 }
