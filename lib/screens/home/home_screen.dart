f674e8c81e7a8a5a4bd56da307b603190784a4a6
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2024-03-31 12:07:34 +0330

    bug fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -27,57 +27,61 @@ class HomeScreen extends StatelessWidget {
       },
       child: Scaffold(
         body: SafeArea(
-          child: BlocBuilder<HomeBloc, HomeState>(builder: ((context, state) {
-            if (state is HomeSuccess) {
-              return ListView.builder(
-                  itemCount: 5,
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 100),
-                  itemBuilder: (context, index) {
-                    switch (index) {
-                      case 0:
-                        return Image.asset(
-                          'assets/images/nike_logo.png',
-                          height: 32,
-                        ).marginOnly(bottom: 16);
-                      case 2:
-                        return BannerSlider(banners: state.banners);
-                      case 3:
-                        return _HorizontalProductList(
-                          title: 'جدیدترین',
-                          onTap: () {},
-                          products: state.latestProducts,
-                        );
-                      case 4:
-                        return _HorizontalProductList(
-                          title: 'پربازدیدترین',
-                          onTap: () {},
-                          products: state.popularProducts,
-                        );
-                      default:
-                        return Container();
-                    }
-                  });
-            } else if (state is HomeLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is HomeError) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(state.exception.message),
-                    ElevatedButton(
-                        onPressed: () {
-                          BlocProvider.of<HomeBloc>(context).add(HomeRefresh());
-                        },
-                        child: const Text('تلاش دوباره')),
-                  ],
-                ),
-              );
-            } else {
-              throw Exception('state is not supported');
-            }
-          })),
+          child: BlocBuilder<HomeBloc, HomeState>(
+            builder: ((context, state) {
+              if (state is HomeSuccess) {
+                return ListView.builder(
+                    itemCount: 5,
+                    padding: const EdgeInsets.only(bottom: 64),
+                    itemBuilder: (context, index) {
+                      switch (index) {
+                        case 0:
+                          return Image.asset(
+                            'assets/images/nike_logo.png',
+                            height: 28,
+                          ).marginSymmetric(vertical: 16);
+                        case 2:
+                          return BannerSlider(banners: state.banners)
+                              .marginOnly(bottom: 32);
+                        case 3:
+                          return _HorizontalProductList(
+                            title: 'جدیدترین',
+                            onTap: () {},
+                            products: state.latestProducts,
+                          );
+                        case 4:
+                          return _HorizontalProductList(
+                            title: 'پربازدیدترین',
+                            onTap: () {},
+                            products: state.popularProducts,
+                          );
+                        default:
+                          return Container();
+                      }
+                    });
+              } else if (state is HomeLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is HomeError) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(state.exception.message),
+                      ElevatedButton(
+                          onPressed: () {
+                            BlocProvider.of<HomeBloc>(context)
+                                .add(HomeRefresh());
+                          },
+                          child: const Text('تلاش دوباره')),
+                    ],
+                  ),
+                );
+              } else {
+                throw Exception('state is not supported');
+              }
+            }),
+          ),
         ),
       ),
     );
@@ -111,74 +115,74 @@ class _HorizontalProductList extends StatelessWidget {
         SizedBox(
           height: 290,
           child: ListView.builder(
-              physics: scrollPhysics,
-              itemCount: products.length,
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 8, right: 8),
-              itemBuilder: ((context, index) {
-                final product = products[index];
-                return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: SizedBox(
-                      width: 176,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+            physics: scrollPhysics,
+            itemCount: products.length,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            itemBuilder: ((context, index) {
+              final product = products[index];
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SizedBox(
+                  width: 176,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Stack(
                         children: [
-                          Stack(
-                            children: [
-                              SizedBox(
-                                width: 176,
-                                height: 189,
-                                child: ImageLoadingService(
-                                  imageUrl: product.imageUrl,
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
+                          SizedBox(
+                            width: 176,
+                            height: 189,
+                            child: ImageLoadingService(
+                              imageUrl: product.imageUrl,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          Positioned(
+                            right: 8,
+                            top: 8,
+                            child: Container(
+                              width: 32,
+                              height: 32,
+                              alignment: Alignment.center,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
                               ),
-                              Positioned(
-                                right: 8,
-                                top: 8,
-                                child: Container(
-                                  width: 32,
-                                  height: 32,
-                                  alignment: Alignment.center,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.white,
-                                  ),
-                                  child: const Icon(CupertinoIcons.heart,
-                                      size: 20),
-                                ),
-                              )
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              product.title,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
+                              child: const Icon(CupertinoIcons.heart, size: 20),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8, left: 8),
-                            child: Text(
-                              product.previousPrice.withPriceLabel,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall!
-                                  .copyWith(
-                                      decoration: TextDecoration.lineThrough),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 8, right: 8, top: 4),
-                            child: Text(product.price.withPriceLabel),
-                          ),
+                          )
                         ],
                       ),
-                    ));
-              })),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          product.title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8, left: 8),
+                        child: Text(
+                          product.previousPrice.withPriceLabel,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(decoration: TextDecoration.lineThrough),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 8, right: 8, top: 4),
+                        child: Text(product.price.withPriceLabel),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+          ),
         )
       ],
     );
